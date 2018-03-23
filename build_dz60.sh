324a191b294022ed6e9248d5cf65829757951c73
FlyingBBQ <derek.cardol@gmail.com>
2018-03-23 21:57:03 +0100

    Keyboard firmware

---
+++ build_dz60.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+board="dz60"
+layout="default"
+chip="atmega32u4"
+
+echo "Making hex file for layout $splitspace on $board"
+make $board:$layout
+
+echo "Flash the board? [y/n]"
+read input
+if [ "$input" == "n" ] || [ "$input" == "N" ]; then
+    exit 1
+fi
+
+echo "Flashing the board in 5 seconds. hit < space + \ > for RESET mode"
+for i in 5 4 3 2 1
+do
+    echo "$i..."
+    sleep 1
+done
+
+echo "Starting..."
+dfu-programmer $chip erase --force
+sleep 1
+dfu-programmer $chip flash "${board}_${layout}.hex"
+sleep 1
+dfu-programmer $chip reset
+
+echo "Finished!"
---
+++ keymap.c
@@ -0,0 +1,67 @@
+#include "dz60.h"
+
+// Layers
+#define _DF 0
+#define _L1 1
+#define _FN 2
+#define _MC 3
+
+// keys
+#define ______ KC_TRNS
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+    // Default with Left and Right Space
+    [_DF] = KEYMAP( KC_GRV  , KC_1    , KC_2    , KC_3   , KC_4    , KC_5   , KC_6    , KC_7    , KC_8  , KC_9    , KC_0     , KC_MINS , KC_EQL  , KC_NO   , KC_BSPC ,
+            KC_TAB  , KC_Q    , KC_W    , KC_E   , KC_R    , KC_T   , KC_Y    , KC_U    , KC_I  , KC_O    , KC_P     , KC_LBRC , KC_RBRC , KC_BSLS ,
+            KC_ESC  , KC_A    , KC_S    , KC_D   , KC_F    , KC_G   , KC_H    , KC_J    , KC_K  , KC_L    , KC_SCLN  , KC_QUOT , KC_ENT  ,
+            KC_LSFT , KC_NO   , KC_Z    , KC_X   , KC_C    , KC_V   , KC_B    , KC_N    , KC_M  , KC_COMM , KC_DOT   , KC_SLSH , KC_RSFT , KC_DEL  ,
+            KC_LCTL , KC_LGUI , KC_LALT , KC_SPC , MO(_FN) , KC_SPC , KC_RALT , OSL(_MC), KC_NO , TO(_L1) , KC_RCTL) ,
+
+    // LeftSpace = BackSpace
+    [_L1] = KEYMAP(
+            KC_GRV  , KC_1    , KC_2    , KC_3   , KC_4    , KC_5   , KC_6    , KC_7    , KC_8  , KC_9    , KC_0     , KC_MINS , KC_EQL  , KC_NO   , KC_BSPC ,
+            KC_TAB  , KC_Q    , KC_W    , KC_E   , KC_R    , KC_T   , KC_Y    , KC_U    , KC_I  , KC_O    , KC_P     , KC_LBRC , KC_RBRC , KC_BSLS ,
+            KC_ESC  , KC_A    , KC_S    , KC_D   , KC_F    , KC_G   , KC_H    , KC_J    , KC_K  , KC_L    , KC_SCLN  , KC_QUOT , KC_ENT  ,
+            KC_LSFT , KC_NO   , KC_Z    , KC_X   , KC_C    , KC_V   , KC_B    , KC_N    , KC_M  , KC_COMM , KC_DOT   , KC_SLSH , KC_RSFT , KC_DEL  ,
+            KC_LCTL , KC_LGUI , KC_LALT , KC_BSPC, MO(_FN) , KC_SPC , KC_RALT , OSL(_MC), KC_NO , TO(_DF) , KC_RCTL) ,
+
+    // Function layer via mid-space modifier key
+    [_FN] = KEYMAP(
+            ______ , KC_F1   , KC_F2   , KC_F3  , KC_F4  , KC_F5  , KC_F6   , KC_F7   , KC_F8   , KC_F9   , KC_F10   , KC_F11  , KC_F12  , ______  , ______ ,
+            ______ , ______  , KC_PGUP , ______ , ______ , ______ , ______  , ______  , ______  , ______  , ______   , ______  , ______  , RESET   ,
+            ______ , KC_HOME , KC_PGDN , KC_END , ______ , ______ , KC_LEFT , KC_DOWN , KC_UP   , KC_RGHT , ______   , ______  , ______  ,
+            ______ , ______  , ______  , ______ , ______ , ______ , KC_MPLY , KC_VOLD , KC_VOLU , KC_MPRV , KC_MNXT  , KC_MUTE , RGB_TOG , KC_PSCR ,
+            ______ , ______  , ______  , ______ , ______ , ______ , RGB_VAD , RGB_VAI , ______  , RGB_M_P , RGB_M_B) ,
+    [_MC] = KEYMAP(
+		    ______ , M(1)   , M(2)   , M(3)   , M(4)   , M(5)   , M(6)   , M(7)   , M(8)   , M(9)   , M(10)   , M(11)  , M(12)  , ______ , ______ ,
+            ______ , ______ , ______ , ______ , ______ , ______ , ______ , ______ , ______ , ______ , ______  , ______ , ______ , ______ ,
+            ______ , ______ , ______ , ______ , ______ , ______ , ______ , ______ , ______ , ______ , ______  , ______ , ______ ,
+            ______ , ______ , ______ , ______ , ______ , ______ , ______ , ______ , ______ , ______ , ______  , ______ , ______ , ______ ,
+            ______ , ______ , ______ , ______ , ______ , ______ , ______ , ______ , ______ , ______ , ______) ,
+};
+
+const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
+    if (record->event.pressed) {
+        switch(id) {
+            case 1:
+                SEND_STRING("https://www.reddit.com/r/MechanicalKeyboards/");
+                break;
+            case 2:
+                rgblight_show_solid_color(69, 133, 136);
+                break;
+            case 3:
+                rgblight_show_solid_color(254, 128, 25);
+                break;
+            case 5:
+                //cyan
+                rgblight_sethsv(183, 49, 52);
+                break;
+            case 6:
+                //orange
+                rgblight_sethsv(27, 90, 100);
+                break;
+        }
+    }
+    return MACRO_NONE;
+}
